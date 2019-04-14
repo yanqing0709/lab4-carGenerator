a33d2524eaa157fc05f8f1cf8f3e61b74baffd9e
Chen Yan Qing <smile8707079@yahoo.com.tw>
2019-04-14 20:48:07 +0800

    Initial Commit

---
+++ lab4-carGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab4-carGenerator
+//
+//  Created by 陳嬿晴 on 2019/4/14.
+//  Copyright © 2019 陳嬿晴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lab4-carGeneratorTests/lab4_carGeneratorTests.swift
@@ -0,0 +1,34 @@
+//
+//  lab4_carGeneratorTests.swift
+//  lab4-carGeneratorTests
+//
+//  Created by 陳嬿晴 on 2019/4/14.
+//  Copyright © 2019 陳嬿晴. All rights reserved.
+//
+
+import XCTest
+@testable import lab4_carGenerator
+
+class lab4_carGeneratorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
